9ba0e5c7ee301e60e6db7431319d4941a753fb69
chuckzeegfs <chuck.zimmerman@gfs.com>
2018-04-05 08:56:37 -0400

    Adding permission requests to config.xml

--- www/js/index.js
+++ www/js/index.js
@@ -29,7 +29,7 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
         document.addEventListener('deviceready', function() {
             $.support.cors=true;
-            barcodeScan();
+            // barcodeScan();
             retrieveCache();
             clickListen();
         });
@@ -46,13 +46,6 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
         console.log('Received Event: ' + id);
 
     }
